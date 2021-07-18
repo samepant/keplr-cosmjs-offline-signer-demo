bfd99fd98b6ad466e3435305f5b9547f47932903
samepant <sampanter@gmail.com>
2021-07-17 21:42:13 -0400

    working example (with my local simapp)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Secp256k1HdWallet } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
-// import { ChainInfo, AppCurrency, Currency, Bech32Config } from "@keplr-wallet/types";
 import { Window as KeplrWindow } from "@keplr-wallet/types";
 import "./App.css";
 
@@ -8,7 +7,7 @@ declare global {
   interface Window extends KeplrWindow {}
 }
 
-const address = "cosmos1vqpjljwsynsn58dugz0w8ut7kun7t8ls2qkmsq";
+const address = "cosmos1q4w6w5lrsqut6mxj49vlvz7hqclsy0tnhrz825";
 const txMsgs = [
   {
     typeUrl: "/cosmos.bank.v1beta1.MsgSend",
@@ -37,7 +36,7 @@ const txMemo = "Tokens from us to you";
 const signerData = {
   accountNumber: 244765,
   sequence: 0,
-  chainId: "cosmoshub-4",
+  chainId: "purp-chain",
 };
 
 function App() {
@@ -45,25 +44,32 @@ function App() {
     "gap way stumble absent siren butter vehicle fancy wait lawn legend wave stomach hospital number";
 
   const cosmjsSign = async () => {
-    const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic);
-    const signingClient = await SigningStargateClient.offline(wallet);
-    const { bodyBytes, signatures } = await signingClient.sign(
-      address,
-      txMsgs,
-      txFee,
-      txMemo,
-      signerData
-    );
+    try {
+      const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic);
+      const signingClient = await SigningStargateClient.offline(wallet);
+      const { bodyBytes, signatures } = await signingClient.sign(
+        address,
+        txMsgs,
+        txFee,
+        txMemo,
+        signerData
+      );
     console.log(bodyBytes, signatures);
+    } catch (error) {
+      console.log('cosmjs sign error: ', error.message)
+    }
   };
 
   const keplrSign = async () => {
     try {
       if (window && window.getOfflineSigner) {
-        const offlineSigner = window.getOfflineSigner("cosmoshub-4");
+        const offlineSigner = window.getOfflineSigner("purp-chain");
         const accounts = await offlineSigner.getAccounts();
         const signingClient = await SigningStargateClient.offline(
-          offlineSigner
+          offlineSigner,
+          {
+
+          }
         );
         const { bodyBytes, signatures } = await signingClient.sign(
           address,
@@ -79,39 +85,69 @@ function App() {
     }
   };
 
-  // const addDummyChain = async () => {
-  //   const res = await window.experimentalSuggestChain({
-  //     rpc: '',
-  //     rest: '',
-  //     chainId: '',
-  //     chainName: '',
-  //     currencies: {
+  const addDummyChain = async () => {
+    try {
+      
+    if (!window.keplr) {
+      alert("Please install keplr extension");
+    } else {
+      const res = await window.keplr.experimentalSuggestChain({
+        chainId: "purp-chain",
+        chainName: "purple test chain",
+        rpc: "http://127.0.0.1:26657",
+        rest: "http://127.0.0.1:1317",
+        bip44: {
+          coinType: 118,
+        },
+        bech32Config: {
+          bech32PrefixAccAddr: "cosmos",
+          bech32PrefixAccPub: "cosmos" + "pub",
+          bech32PrefixValAddr: "cosmos" + "valoper",
+          bech32PrefixValPub: "cosmos" + "valoperpub",
+          bech32PrefixConsAddr: "cosmos" + "valcons",
+          bech32PrefixConsPub: "cosmos" + "valconspub",
+        },
+        currencies: [
+          {
+            coinDenom: "stake",
+            coinMinimalDenom: "stake",
+            coinDecimals: 6,
+          },
+        ],
+        feeCurrencies: [
+          {
+            coinDenom: "stake",
+            coinMinimalDenom: "stake",
+            coinDecimals: 6,
+          },
+        ],
+        stakeCurrency: {
+          coinDenom: "stake",
+          coinMinimalDenom: "stake",
+          coinDecimals: 6,
+        },
+        coinType: 118,
+        gasPriceStep: {
+          low: 1,
+          average: 1,
+          high: 1,
+        },
+      });
 
-  //     },
-  //     stakeCurrency: {
-  //       coinDenom: 'stake',
-  //       coinMinimalDenom: '',
-  //       coinDecimals: 0,
-  //     },
-  //     bip44: {
-  //       coinType: 0,
-  //     },
-  //     bech32Config: {
-  //       bech32PrefixAccAddr: '',
-  //       bech32PrefixAccPub: '',
-  //       bech32PrefixValAddr: '',
-  //       bech32PrefixValPub: '',
-  //       bech32PrefixConsAddr: '',
-  //       bech32PrefixConsPub: '',
-  //     },
-  //   });
-  // }
+      console.log(res);
+    }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <p>Import this secret to your keplr wallet:</p>
         <code>{mnemonic}</code>
-        <button onClick={cosmjsSign}>Sign with cosmjs</button>
+        <button onClick={cosmjsSign}>Sign with cosmjs (logs to console)</button>
+        <button onClick={keplrSign}>Sign with keplr offlinse signer (logs to console)</button>
+        <button onClick={addDummyChain}>Add local simd chain</button>
       </header>
     </div>
   );
